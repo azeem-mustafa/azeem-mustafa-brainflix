0cc8ee6ff95a0e470811ef38d803d0ff8bc929a6
Azeem Mustafa <azeem.mustafa.8574@gmail.com>
2021-08-19 15:59:52 -0400

    created rough layout of header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import LogoImg from './assets/Logo/Logo-brainflix.svg'
+import Avatar from './assets/images/Mohan-muruge.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <div className= "header">
+        <img className= 'header__img' src= {LogoImg} alt="logo-img" />
+        <input className= "header__search-bar" type= "text" placeholder= "Search"/>
+
+        <div className= "header__upload-box">
+          <button className="header__button" type="submit">UPLOAD</button>
+
+          <div className="header__img-box">
+              <img className="header__avatar" src= {Avatar} alt="mohan-img"/>
+          </div>
+
+        </div>
+
+      </div>
+
+    </header>
+
   );
 }
 
